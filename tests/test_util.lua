2d55451a47573554e11a4c045872633782ca2e38
Steven Arcangeli <stevearc@stevearc.com>
2023-03-05 20:01:06 -0800

    test: add tests and run them in ci

---
+++ tests/test_util.lua
@@ -0,0 +1,17 @@
+require("plenary.async").tests.add_to_env()
+local M = {}
+
+M.reset_editor = function()
+  vim.cmd.tabonly({ mods = { silent = true } })
+  for i, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if i > 1 then
+      vim.api.nvim_win_close(winid, true)
+    end
+  end
+  vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(false, true))
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    vim.api.nvim_buf_delete(bufnr, { force = true })
+  end
+end
+
+return M
